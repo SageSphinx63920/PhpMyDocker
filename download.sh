e7dd8d9b677465cbed19c3a79ca4036792ebdd99
SageSphinx63920 <sage@sagesphinx63920.dev>
2023-03-11 19:34:58 +0100

    Init

---
+++ download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the URL for the latest version of PhpMyAdmin
+PMA_URL="https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz"
+
+# Define the directory where PhpMyAdmin will be installed
+PMA_DIR="./data/web/"
+
+# Download the latest version of PhpMyAdmin
+curl -o ./data/web/phpmyadmin.tar.gz $PMA_URL
+
+# Extract the PhpMyAdmin archive to the installation directory
+tar -xzf ./data/web/phpmyadmin.tar.gz -C $PMA_DIR --strip-components=1
+
+# Clean up the downloaded archive file
+rm ./data/web/phpmyadmin.tar.gz
